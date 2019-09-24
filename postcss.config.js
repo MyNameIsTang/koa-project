2694996d04f75fe7adef2843b46fa373e51a8cdd
tangdaguang <daguang.tang@chaitin.com>
2019-09-24 18:51:03 +0800

    fixed:project structure

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,18 @@
 const plugins = [
-    require('autoprefixer')({
-
-    }),
-    require('cssnano')({
-        preset: [
-            'default', {
-                discardComments: {
-                    removeAll: true
-                }
-            }
-        ]
-    })
+  require('autoprefixer')({
+    
+  }),
+  require('cssnano')({
+    preset: [
+      'default', {
+        discardComments: {
+          removeAll: true
+        }
+      }
+    ]
+  })
 ]
 
 module.exports = {
-    plugins
+  plugins
 }
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -4,8 +4,8 @@ import * as Router from 'koa-router'
 const router: Router = new Router()
 
 router.get('/', async (ctx: Koa.Context) => {
-    await ctx.render('blog', {
-        title: '博客'
+    await ctx.render('index', {
+        title: '123',
     })
 })
 
